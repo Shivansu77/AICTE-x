58234be155d3a7850a1079a39fc124bfb370d7e1
shivansu77 <shivansubisht77@gmail.com>
2026-02-03 22:35:30 +0530

    Update API URL for production deployment

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: import.meta.env.PROD 
+        ? 'https://aicte-x.onrender.com/api' 
+        : 'http://localhost:5001/api',
     headers: {
         'Content-Type': 'application/json',
     },
